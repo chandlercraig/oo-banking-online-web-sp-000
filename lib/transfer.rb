f292569be8fe05532887c6903c9ec662451dcf98
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-10 18:21:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,17 +21,29 @@ class Transfer
     sender.valid? && receiver.valid?
   end
 
-  def execute_transaction
+  # def execute_transaction
     # binding.pry
-    if self.valid? && self.status != "complete"
-      sender.withdraw(amount)
-      receiver.deposit(amount)
-      self.status = "complete"
-    elsif sender.balance < amount
-      self.status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+  #   if self.valid? && self.status != "complete"
+  #     sender.withdraw(amount)
+  #     receiver.deposit(amount)
+  #     self.status = "complete"
+  #   elsif sender.balance < amount
+  #     self.status = "rejected"
+  #     return "Transaction rejected. Please check your account balance."
+  #   end
+  # end
+
+    def execute_transaction
+      if sender.balance < amount
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      elsif @status != "complete" && sender.valid?
+        @receiver.balance = @receiver.balance + amount
+        @sender.balance = @sender.balance - amount
+        @status = "complete"
+      end
     end
-  end
+
 
   def reverse_transfer
     if valid? && @receiver.balance > amount && status == "complete"
